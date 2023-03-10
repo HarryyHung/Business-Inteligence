f84a17d0683543a24913270eabc8bcbd48c5c419
hanguyen2002v <109323029+hanguyen2002v@users.noreply.github.com>
2023-03-10 14:23:59 +0700

    Giải thích code
    Dm thang hung

--- ProjectPython/data.py
+++ ProjectPython/data.py
@@ -1,17 +1,25 @@
 import csv
 
 file = open("raw_titles1.csv", "r", encoding="utf-8-sig")
+#Functions reader from library
 reader = csv.reader(file)
+#Heading in the csv
 header = next(reader)
 
+#Delete column "id" (header)
 del header[1]
+#Delete column "imdb_id" (header)
 del header[9]
+#Add column "Quantity Genres" (header)
 header.append("Quantity Genre")
 
 file_new = open("raw_titles_new1.csv", "w", encoding="utf-8-sig", newline='')
-writer = csv.writer(file_new )
+#Function writer from library
+writer = csv.writer(file_new)
+# Write header in the clean csv
 writer.writerow(header)
 
+#Create a dictionary of age certificate for each film's labels
 age_dict = {"TV-MA": "18+", "R": "17+",
             "NC-17": "17-18+", "PG": "18+",
             "TV-PG": "18+", "TV-14": "14+",
@@ -27,10 +35,14 @@ age_dict = {"TV-MA": "18+", "R": "17+",
 #               "IE": "Ireland", "GH": "Ghana",
 #               }
 
+# Use for each row in the csv file
 for row in reader:
+    #Delete column "id" (row)
     del row[1]
+    #Delete column "imdb_id" (row)
     del row[9]
 
+    #Check the above rows, if empty replace it with 0
     if row[8] == "":
         row[8] = '0'
     if row[4] == "":
@@ -40,36 +52,46 @@ for row in reader:
     if row[10] == "":
         row[10] = '0'
 
-    # Fix name
-    title = row[1].capitalize()
-    row[1] = title
+    # Fix name, Use the python capitalize() method to return a string where the 
+    # first character is uppercase and the rest is lowercase.
+    name_film = row[1].capitalize()
+    row[1] = name_film
 
-    # Convert to int
-    row[8] = int(float(row[8]))
-    row[9] = int(float(row[9]))
-    row[10] = int(float(row[10]))
+    # Convert the above rows from string to int
+    row[8] = int(float(row[8])) # row "seasons"
+    row[9] = int(float(row[9])) # row "imdb_score"
+    row[10] = int(float(row[10])) # row "imdb_votes"
 
-    # Remove "[]"
+    # Remove "['']" and "\'" use strip() and replace()
     genres = row[6].strip("['']").replace("\'", '')
     row[6] = genres
 
+    # Remove "[]" and "\'" use strip() and replace()
+    # Use replace() to change the abbreviations of a country name to a specific country name
     product_count = row[7].replace("\'", '').strip("[]")
     row[7] = product_count.replace("BS", "Bahamas").replace("FO", "Faroe Islands").replace("NZ", "New Zealand").replace("GR", "Greece").replace("US", "The United States").replace("GB","The United Kingdom").replace("BF", "Burkina Faso").replace("AU", "Australia").replace("JP", "Japan").replace("EG", "Egypt").replace("DE", "Germany").replace("ES", "Spain").replace("MX", "Mexico").replace("CO", "Colombia").replace("IN", "India").replace("DZ", "Algeria").replace("LB", "Lebanon").replace("BE", "Belgium").replace("NO", "Na Uy").replace("FR", "France").replace("CA", "Canada").replace("SU", "The Soviet Union").replace("PS", "Palestine").replace("TR", "Turkey").replace("IT", "Italy").replace("BR", "Brasil").replace("PE", "Peru").replace("HK", "Hong Kong").replace("AR", "Argentina").replace("IE", "Ireland").replace("GH", "Ghana").replace("KR", "Korea").replace("BG", "Bulgaria").replace("CN", "People 'Republic of China").replace("RU", "Russia").replace("SG", "Singapore").replace("MA", "Roma").replace("UY", "Na Uy").replace("TW", "Taiwan").replace("DK", "Denmark").replace("MY", "Malaysia").replace("CH","China").replace("CL", "Chile").replace("KW", "Kuwait").replace("NG", "Nigeria").replace("ZA", "South Africa").replace("SA", "Saudi Arabia").replace("AT", "Austria").replace("NL", "Netherlands").replace("SE", "Sweden").replace("CZ", "Czech Republic").replace("PH", "Philippines").replace("TH", "Thailand").replace("AE", "United Arab Emirates").replace("HU", "Hungary").replace("ID", "Indonesia").replace("IL", "Israel").replace("FI", "Finland").replace("RO", "Romania").replace("CD", "Democratic Republic of the Congo").replace("PL", "Poland").replace("VE", "Venezuela").replace("IS", "Iceland").replace("NZ", "New Zealand").replace("UA", "Ukraine").replace("IR", "Iran").replace("JO", "Jordan").replace("QA", "Qatar").replace("HR", "Croatia").replace("SY", "Syria").replace("VN", "Vietnam").replace("PR", "Puerto Rico").replace("IQ", "Iraq").replace("KH", "Cambodia").replace("CU", "Cuba").replace("LU", "Luxembourg").replace("KE", "Kenya").replace("RS", "Serbia").replace("AL", "Albania").replace("TZ", "Tanzania").replace("GE", "Georgia").replace("ZW", "Zimbabwe").replace("CM", "Cameroon").replace("PK", "Pakistan").replace("BD", "Bangladesh").replace("CY", "Cyprus").replace("LT", "Lithuania").replace("MW", "Malawi").replace("PT", "Portugal").replace("AO", "Angola").replace("GT", "Guatemala").replace("MZ", "Mozambique").replace("AF", "Afghanistan").replace("NA", "Namibia").replace("IO", "British Indian Ocean Territory")
 
+    # age_certification change to number by age
+    # Use for each to iterate over each item in "row" where "index" is the index and "value" is the value of each item. 
+    # And the enumerate() function is used to add a counter for the index to the list
+    for index, value in enumerate(row):
+        # print(f'{index}: {value}')
+        # Check if the value is in the pre-initialized age dictionary
+        if value in age_dict:
+            # Replace any age value found in "row" with the value in "age_dict"
+            row[index] = age_dict[value]
+
     # Covert str to list
     str = row[6].split()
-
-    # age_certification đổi sang số theo độ tuổi and country đổi thành tên nước cụ thể
-    for index, value in enumerate(row):
-        if value in age_dict:
-            row[index] = age_dict[value]
-    
-    # thêm cột số lượng genre
+    # Get the number in each genre
     quantity_genre = len(str)
+    #add quantity genre number row
     row.append(quantity_genre)
-
+    
+    # write row in the clean csv
     writer.writerow(row)
 
 print("Done")
+# close old file and new file
 file.close()
 file_new.close()
